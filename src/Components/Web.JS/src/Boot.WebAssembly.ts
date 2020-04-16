f0002bb5eb49a177e38571f89c4e975ccb43a4da
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-04-16 12:26:15 +0100

    Define WebAssemblyStartOptions

--- src/Components/Web.JS/src/Boot.WebAssembly.ts
+++ src/Components/Web.JS/src/Boot.WebAssembly.ts
@@ -10,10 +10,11 @@ import { WebAssemblyResourceLoader } from './Platform/WebAssemblyResourceLoader'
 import { WebAssemblyConfigLoader } from './Platform/WebAssemblyConfigLoader';
 import { BootConfigResult } from './Platform/BootConfig';
 import { Pointer } from './Platform/Platform';
+import { WebAssemblyStartOptions } from './Platform/WebAssemblyStartOptions';
 
 let started = false;
 
-async function boot(options?: any): Promise<void> {
+async function boot(options?: Partial<WebAssemblyStartOptions>): Promise<void> {
 
   if (started) {
     throw new Error('Blazor has already started.');
